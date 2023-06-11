d44ff0a372272587b6b38870a6288e9f5138550d
Cristian Crețu <crisemcr@gmail.com>
2023-06-11 17:31:37 +0300

    Initial Commit

---
+++ design/designApp.swift
@@ -0,0 +1,17 @@
+//
+//  designApp.swift
+//  design
+//
+//  Created by Cristian Cretu on 11.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
